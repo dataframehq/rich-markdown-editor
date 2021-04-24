72bdd1eff134feb8ffff205eb1a094c5205108ab
Kevin Kong <kevinkong91@gmail.com>
2021-04-24 16:28:06 -0400

    chore: remove QueryFence & QueryBlock

--- dist/nodes/QueryBlock.d.ts
+++
@@ -1,6 +0,0 @@
-import QueryFence from "./QueryFence";
-export default class QueryBlock extends QueryFence {
-    get name(): string;
-    get markdownToken(): string;
-}
-//# sourceMappingURL=QueryBlock.d.ts.map
