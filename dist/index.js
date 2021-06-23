7951d25f8dbb11cf5115ad01a58b963be02ceb6e
Unini <unini056378@gmail.com>
2021-06-23 14:48:40 -0700

    chore: build

--- dist/index.js
+++ dist/index.js
@@ -506,9 +506,13 @@ const StyledEditor = styled_components_1.default("div") `
     -webkit-font-variant-ligatures: none;
     font-variant-ligatures: none;
     font-feature-settings: "liga" 0; /* the above doesn't seem to work in Edge */
+
+    p br {
+      display: none;
+    }
   }
 
-  pre {
+  .ProseMirror pre {
     white-space: pre-wrap;
   }
 
--- src/index.tsx
+++ src/index.tsx
@@ -722,9 +722,13 @@ const StyledEditor = styled("div")<{
     -webkit-font-variant-ligatures: none;
     font-variant-ligatures: none;
     font-feature-settings: "liga" 0; /* the above doesn't seem to work in Edge */
+
+    p br {
+      display: none;
+    }
   }
 
-  pre {
+  .ProseMirror pre {
     white-space: pre-wrap;
   }
 
